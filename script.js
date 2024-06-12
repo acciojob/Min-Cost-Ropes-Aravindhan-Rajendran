68ef6ab62d9b6c920b97bf65e6cea2bdff43c8fb
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-12 13:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let arr=[];
-let n=arr.length;
-arr.sort((a, b) => a - b);
-let sum=0;
 function mincost(arr)
 { 
-arr.map(({
-	sum=sum+arr[i];
-})
-		);
-	console.log(sum);
+  while (arr.length > 1) {
+    // Connect the two shortest ropes
+    let rope1 = arr.shift();
+    let rope2 = arr.shift();
+    let newRope = rope1 + rope2;
+	   cost += newRope;
+	   let i = arr.findIndex(r => r > newRope);
+    if (i === -1) {
+      // If the new rope is the longest, add it to the end
+      arr.push(newRope);
+    } else {
+      // Otherwise, insert it in the correct position
+      arr.splice(i, 0, newRope);
+    }
+  }
+  
+  return cost;
 }
 
 module.exports=mincost;
